ffa41eb1bf18e3fe08d63596182e4183dd819630
enerzilius <efblouback@gmail.com>
2025-11-23 19:54:42 -0300

    fucking finally

--- src/QueryProcessor.cpp
+++ src/QueryProcessor.cpp
@@ -5,6 +5,7 @@
 #include <filesystem>
 #include <iostream>
 #include <unordered_set>
+#include <unordered_map>
 
 void QueryProcessor::buscar(std::vector<std::string> consulta) {
     std::string path = "index.dat";
@@ -18,14 +19,29 @@ void QueryProcessor::buscar(std::vector<std::string> consulta) {
     std::cout<<" [*] Se você adicionou, removeu ou alterou algum arquivo do diretório: "<<index.directory<<" refaça o build para evitar erros. \n";
 
     std::unordered_set<int> idsMap;
-    std::vector<int> intersection = index.filesMap.find(consulta[2])->second;
-    for (int id : intersection) idsMap.insert(id);
-    for(int i = 3; i < consulta.size(); i++) {
-        intersection.clear();
-        std::vector<int> idsVector = index.filesMap.find(consulta[i])->second;
-        for(int id : idsVector) if(idsMap.count(id) > 0) intersection.push_back(id);
-        idsMap.clear(); 
-        for(int id : intersection) idsMap.insert(id);
+    bool first = true;
+    for(int i = 2; i < consulta.size(); i++) {
+        auto find = index.filesMap.find(consulta[i]);
+        if(find == index.filesMap.end()) {
+            idsMap.clear();
+            break;
+        }
+
+        std::vector<int> idsVector = find->second;
+
+        if (first) {
+            for (int id : idsVector) idsMap.insert(id);
+            first = false;
+            continue;
+        }
+
+        std::vector<int> intersection = {};
+        for(int id : idsVector) if(idsMap.count(id) > 0) intersection.push_back(id);  
+
+        idsMap.clear();
+        for (int id : intersection) idsMap.insert(id);
+
+        if (idsMap.empty()) break; 
     }
 
     if(idsMap.empty()) {
